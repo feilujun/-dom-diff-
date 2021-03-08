aa89535736b4646b089c89ea13da5225623bbb72
feilujun <2454450087@qq.com>
2021-03-08 17:45:47 +0800

    1

---
+++ src/test/h.js
@@ -0,0 +1,9 @@
+import vnode from '../mysnabbdom/vnode'
+
+//  { sel, data, children, text, elm }
+export default function(sel, data, ex) {
+    if(arguments.length !== 3) {
+        throw new Error('参数只能为三个')
+    }
+
+}
